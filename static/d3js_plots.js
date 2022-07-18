32ffffc9e3f4e42d0a80dfecd3592d1f0b45900e
peter <p.makretskii@gmail.com>
2022-07-19 01:49:10 +0300

    make d3 page better; other things

--- static/d3js_plots.js
+++ static/d3js_plots.js
@@ -5,7 +5,7 @@ $(function () {
         {
             let data = get_data('hist', 100);
             begin = Date.now();
-            draw_d3_hist('#hist-d3js-2', data, {
+            draw_d3_hist('#hist-d3js-2', '#nbin-input-d3js-2', '#nbin-d3js-2', data, {
                 title: 'D3js hist for 100 values',
                 xaxis: 'values',
                 yaxis: 'samples'
@@ -15,7 +15,7 @@ $(function () {
 
             data = get_data('hist', 1000);
             begin = Date.now();
-            draw_d3_hist('#hist-d3js-3', data, {
+            draw_d3_hist('#hist-d3js-3', '#nbin-input-d3js-3', '#nbin-d3js-3', data, {
                 title: 'D3js hist for 1000 values',
                 xaxis: 'values',
                 yaxis: 'samples'
@@ -25,7 +25,7 @@ $(function () {
 
             data = get_data('hist', 10000);
             begin = Date.now();
-            draw_d3_hist('#hist-d3js-4', data, {
+            draw_d3_hist('#hist-d3js-4', '#nbin-input-d3js-4', '#nbin-d3js-4', data, {
                 title: 'D3js hist for 10000 values',
                 xaxis: 'values',
                 yaxis: 'samples'
@@ -150,7 +150,7 @@ $(function () {
     }
 )
 
-function draw_d3_hist(id, data, layout) {
+function draw_d3_hist(id, slider_id, slider_div_id, data, layout) {
     const margin = {top: 20, right: 30, bottom: 40, left: 60};
     const width = 1700 - margin.left - margin.right;
     const height = 700 - margin.top - margin.bottom;
@@ -186,7 +186,7 @@ function draw_d3_hist(id, data, layout) {
         .attr("x", -margin.top - 200)
         .text(layout.yaxis);
 
-    svg.append("g")
+    let xAxis = svg.append("g")
         .attr("transform", `translate(0, ${height})`)
         .call(d3.axisBottom(x));
 
@@ -257,6 +257,40 @@ function draw_d3_hist(id, data, layout) {
                 .style("opacity", 0)
         };
 
+        let clip = svg.append("defs").append("SVG:clipPath")
+            .attr("id", "clip")
+            .append("SVG:rect")
+            .attr("width", width)
+            .attr("height", height)
+            .attr("x", 0)
+            .attr("y", 0);
+
+        // Create the scatter variable: where both the circles and the brush take place
+        let scatter = svg.append('g')
+            .attr("clip-path", "url(#clip)")
+
+        // Add circles
+        scatter
+            .selectAll("rect")
+            .data(data)
+            .enter()
+            .append("circle")
+            .attr("cx", function (d) {
+                return x(d.x1);
+            })
+            .attr("cy", function (d) {
+                return y(d.x0);
+            })
+            .attr("r", 8)
+            .style("fill", "#61a3a9")
+            .style("opacity", 0.5)
+
+        // Set the zoom and Pan features: how much you can zoom, on which part, and what to do when there is a zoom
+        let zoom = d3.zoom()
+            .scaleExtent([.5, 20])  // This control how much you can unzoom (x0.5) and zoom (x20)
+            .extent([[0, 0], [width, height]])
+            .on("zoom", updateChart);
+
         // append the bar rectangles to the svg element
         svg.selectAll("rect")
             .data(bins)
@@ -273,9 +307,38 @@ function draw_d3_hist(id, data, layout) {
             })
             .style("fill", "#334de8")
             .style("opacity", "0.75")
+            // .attr("d", areaGenerator)
             .on("mouseover", showTooltip)
             .on("mousemove", moveTooltip)
             .on("mouseleave", hideTooltip);
+
+        svg.append("rect")
+            .attr("width", width)
+            .attr("height", height)
+            .style("fill", "none")
+            .style("pointer-events", "all")
+            .attr('transform', 'translate(' + margin.left + ',' + margin.top + ')')
+            .call(zoom);
+
+        function updateChart(event, d) {
+            // recover the new scale
+            let newX = event.transform.rescaleX(x);
+            let newY = event.transform.rescaleY(y);
+
+            // update axes with these new boundaries
+            xAxis.call(d3.axisBottom(newX))
+            yAxis.call(d3.axisLeft(newY))
+
+            // update circle position
+            scatter
+                .selectAll("rect")
+                .attr('cx', function (d) {
+                    return newX(d.x1)
+                })
+                .attr('cy', function (d) {
+                    return newY(d.x0)
+                });
+        }
     }
 
     make_by_bins(21);  // число бинов по умолчанию
@@ -292,52 +355,10 @@ function draw_d3_hist(id, data, layout) {
         .attr("x2", width)
         .attr("y2", 0);
 
-    $('#id-nbin').show();
-
-    d3.select("#id-nbin-input").on("change", function () {
-        $('#id-nbin label').text('число бинов: ' + this.value);
+    d3.select(slider_id).on("change", function () {
+        $(slider_div_id + ' label').text('число бинов: ' + this.value);
         make_by_bins(+this.value);
     });
-
-    let brush = d3.brush().on("end", brushended);
-    let idleTimeout;
-    let idleDelay = 350;
-    let svg_ = d3.select('svg');
-
-    svg_.append("g")
-        .attr("class", "brush")
-        .call(brush);
-
-    function brushended() {
-        let s = event.selection;
-        if (!s) {
-            if (!idleTimeout) return idleTimeout = setTimeout(idled, idleDelay);
-            x.domain([1940, 1995]);
-            y.domain([0, 80]);
-        } else {
-            x.domain([s[0][0], s[1][0]].map(x.invert, x));
-            y.domain([s[1][1], s[0][1]].map(y.invert, y));
-            svg_.select(".brush").call(brush.move, null);
-        }
-        zoom();
-    }
-
-    function idled() {
-        idleTimeout = null;
-    }
-
-    function zoom() {
-        let t = svg_.transition().duration(750);
-        svg_.select(".axis--x").transition(t).call(x);
-        svg_.select(".axis--y").transition(t).call(y);
-        svg_.selectAll("rect").transition(t)
-            .attr("x", function (d) {
-                return x(d[0]);
-            })
-            .attr("y", function (d) {
-                return y(d[1]);
-            });
-    }
 }
 
 function draw_d3_scatter(id, data, layout) {
@@ -367,7 +388,7 @@ function draw_d3_scatter(id, data, layout) {
         })])
         .range([0, width]);
 
-    svg.append("g")
+    let xAxis = svg.append("g")
         .attr("transform", "translate(0," + height + ")")
         .call(d3.axisBottom(x));
 
@@ -380,7 +401,7 @@ function draw_d3_scatter(id, data, layout) {
         })])
         .range([height, 0]);
 
-    svg.append("g")
+    let yAxis = svg.append("g")
         .attr('class', 'yAxis')
         .call(d3.axisLeft(y));
 
@@ -405,9 +426,51 @@ function draw_d3_scatter(id, data, layout) {
         .attr("x", width / 2)
         .text(layout.title);
 
-    // Add the points
-    svg.append("g")
-        .selectAll("dot")
+    const tooltip = d3.select(id)
+        .append("div")
+        .style("opacity", 0)
+        .attr("class", "tooltip")
+        .style("background-color", "white")
+        .style("border", "solid")
+        .style("border-width", "1px")
+        .style("border-radius", "5px")
+        .style("padding", "10px")
+
+    // A function that change this tooltip when the user hover a point.
+    // Its opacity is set to 1: we can now see it. Plus it set the text and position of tooltip depending on the datapoint (d)
+    const mouseover = function (event, d) {
+        tooltip
+            .style("opacity", 1)
+    }
+
+    const mousemove = function (event, d) {
+        tooltip
+            .html(d)
+            .style("left", (event.x) / 2 + "px") // It is important to put the +90: other wise the tooltip is exactly where the point is an it creates a weird effect
+            .style("top", (event.y) / 2 + "px")
+    }
+
+    // A function that change this tooltip when the leaves a point: just need to set opacity to 0 again
+    const mouseleave = function (event, d) {
+        tooltip
+            .transition()
+            .duration(200)
+            .style("opacity", 0)
+    }
+
+    svg.append("defs").append("SVG:clipPath")
+        .attr("id", "clip")
+        .append("SVG:rect")
+        .attr("width", width)
+        .attr("height", height)
+        .attr("x", 0)
+        .attr("y", 0);
+
+    // Create the scatter variable: where both the circles and the brush take place
+    let scatter = svg.append('g')
+
+    scatter
+        .selectAll("circle")
         .data(data_format)
         .enter()
         .append("circle")
@@ -418,7 +481,48 @@ function draw_d3_scatter(id, data, layout) {
             return y(d[1]);
         })
         .attr("r", 5)
-        .attr("fill", "#334de8");
+        .style("fill", "#334de8")
+        .style("opacity", 0.5)
+
+    // Set the zoom and Pan features: how much you can zoom, on which part, and what to do when there is a zoom
+    let zoom = d3.zoom()
+        .scaleExtent([.5, 20])  // This control how much you can unzoom (x0.5) and zoom (x20)
+        .extent([[0, 0], [width, height]])
+        .on("zoom", updateChart);
+
+    // This add an invisible rect on top of the chart area. This rect can recover pointer events: necessary to understand when the user zoom
+    svg.append("rect")
+        .attr("width", width)
+        .attr("height", height)
+        .style("fill", "none")
+        .style("pointer-events", "all")
+        .attr('transform', 'translate(' + margin.left + ',' + margin.top + ')')
+        .call(zoom);
+    // now the user can zoom and it will trigger the function called updateChart
+
+    // A function that updates the chart when the user zoom and thus new boundaries are available
+    function updateChart(event, d) {
+        // recover the new scale
+        let newX = event.transform.rescaleX(x);
+        let newY = event.transform.rescaleY(y);
+        // update axes with these new boundaries
+        xAxis.call(d3.axisBottom(newX));
+        yAxis.call(d3.axisLeft(newY));
+        // update circle position
+        scatter
+            .selectAll("circle")
+            .attr('cx', function (d) {
+                return newX(d[0])
+            })
+            .attr('cy', function (d) {
+                return newY(d[1])
+            });
+    }
+
+    svg
+        .on("mouseover", mouseover)
+        .on("mousemove", mousemove)
+        .on("mouseleave", mouseleave);
 
     // grid
     d3.selectAll("g.yAxis g.tick")
@@ -460,7 +564,7 @@ function draw_d3_scatter_with_lines(id, data, layout) {
         })])
         .range([0, width]);
 
-    svg.append("g")
+    let xAxis = svg.append("g")
         .attr("transform", "translate(0," + height + ")")
         .call(d3.axisBottom(x));
 
@@ -473,7 +577,7 @@ function draw_d3_scatter_with_lines(id, data, layout) {
         })])
         .range([height, 0]);
 
-    svg.append("g")
+    let yAxis = svg.append("g")
         .attr('class', 'yAxis')
         .call(d3.axisLeft(y));
 
@@ -508,16 +612,66 @@ function draw_d3_scatter_with_lines(id, data, layout) {
 
     svg.append('path')
         .datum(data_format)
+        .attr("class", "myArea")
         .attr("fill", "none")
         .attr("stroke", "steelblue")
         .attr("stroke-linejoin", "round")
         .attr("stroke-linecap", "round")
         .attr("stroke-width", 1.5)
-        .attr("d", line);
+        .attr("d", line)
+        .attr("cx", function (d) {
+            return x(d[0]);
+        })
+        .attr("cy", function (d) {
+            return y(d[1]);
+        });
 
-    // Add the points
-    svg.append("g")
-        .selectAll("dot")
+    const tooltip = d3.select(id)
+        .append("div")
+        .style("opacity", 0)
+        .attr("class", "tooltip")
+        .style("background-color", "white")
+        .style("border", "solid")
+        .style("border-width", "1px")
+        .style("border-radius", "5px")
+        .style("padding", "10px")
+
+
+    // A function that change this tooltip when the user hover a point.
+    // Its opacity is set to 1: we can now see it. Plus it set the text and position of tooltip depending on the datapoint (d)
+    const mouseover = function (event, d) {
+        tooltip
+            .style("opacity", 1)
+    }
+
+    const mousemove = function (event, d) {
+        tooltip
+            .html(d)
+            .style("left", (event.x) / 2 + "px") // It is important to put the +90: other wise the tooltip is exactly where the point is an it creates a weird effect
+            .style("top", (event.y) / 2 + "px")
+    }
+
+    // A function that change this tooltip when the leaves a point: just need to set opacity to 0 again
+    const mouseleave = function (event, d) {
+        tooltip
+            .transition()
+            .duration(200)
+            .style("opacity", 0)
+    }
+
+    svg.append("defs").append("SVG:clipPath")
+        .attr("id", "clip")
+        .append("SVG:rect")
+        .attr("width", width)
+        .attr("height", height)
+        .attr("x", 0)
+        .attr("y", 0);
+
+    // Create the scatter variable: where both the circles and the brush take place
+    let scatter = svg.append('g')
+
+    scatter
+        .selectAll("circle")
         .data(data_format)
         .enter()
         .append("circle")
@@ -528,7 +682,88 @@ function draw_d3_scatter_with_lines(id, data, layout) {
             return y(d[1]);
         })
         .attr("r", 5)
-        .attr("fill", "#334de8");
+        .style("fill", "#334de8")
+        .style("opacity", 0.5)
+
+    // Set the zoom and Pan features: how much you can zoom, on which part, and what to do when there is a zoom
+    let zoom = d3.zoom()
+        .scaleExtent([.5, 20])  // This control how much you can unzoom (x0.5) and zoom (x20)
+        .extent([[0, 0], [width, height]])
+        .on("zoom", updateChart);
+
+    // This add an invisible rect on top of the chart area. This rect can recover pointer events: necessary to understand when the user zoom
+    svg.append("rect")
+        .attr("width", width)
+        .attr("height", height)
+        .style("fill", "none")
+        .style("pointer-events", "all")
+        .attr('transform', 'translate(' + margin.left + ',' + margin.top + ')')
+        .call(zoom);
+    // now the user can zoom and it will trigger the function called updateChart
+
+    // A function that updates the chart when the user zoom and thus new boundaries are available
+    function updateChart(event, d) {
+        // recover the new scale
+        let newX = event.transform.rescaleX(x);
+        let newY = event.transform.rescaleY(y);
+        // update axes with these new boundaries
+        xAxis.call(d3.axisBottom(newX));
+        yAxis.call(d3.axisLeft(newY));
+        // update circle position
+        scatter
+            .selectAll("circle")
+            .attr('cx', function (d) {
+                return newX(d[0])
+            })
+            .attr('cy', function (d) {
+                return newY(d[1])
+            });
+    //
+    //         let line = d3.line()
+    //     .x(function (d) {
+    //         return x(d[0])
+    //     })
+    //     .y(function (d) {
+    //         return y(d[1])
+    //     });
+    //
+    // svg.append('path')
+    //     .datum(data_format)
+    //     .attr("class", "myArea")
+    //     .attr("fill", "none")
+    //     .attr("stroke", "steelblue")
+    //     .attr("stroke-linejoin", "round")
+    //     .attr("stroke-linecap", "round")
+    //     .attr("stroke-width", 1.5)
+    //     .attr("d", line);
+
+        scatter
+            .selectAll("path")
+            .attr('cx', function (d) {
+                return newX(d[0])
+            })
+            .attr('cy', function (d) {
+                return newY(d[1])
+            });
+    }
+
+    // // Add the points
+    // svg.append("g")
+    //     .selectAll("dot")
+    //     .data(data_format)
+    //     .enter()
+    //     .append("circle")
+    //     .attr("cx", function (d) {
+    //         return x(d[0]);
+    //     })
+    //     .attr("cy", function (d) {
+    //         return y(d[1]);
+    //     })
+    //     .attr("r", 5)
+    //     .attr("fill", "#334de8")
+    //     .on("mouseover", mouseover)
+    //     .on("mousemove", mousemove)
+    //     .on("mouseleave", mouseleave);
 
     // grid
     d3.selectAll("g.yAxis g.tick")
